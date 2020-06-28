fef106b9e142e8ed4a1733575cbb230dc13759e5
Dayan-Zhanchi <harry_95_v@hotmail.com>
2020-06-28 20:47:41 +0200

    Add example use of dataset creations

--- mogrify.py
+++ mogrify.py
@@ -5,38 +5,25 @@ from tensorflow import keras
 import datasets
 
 
-### Paths
+ptb = datasets.PTB()
+ptb_char_train = ptb.char_train
+ptb_char_valid = ptb.char_valid
+ptb_word_train = ptb.word_train
+ptb_word_valid = ptb.word_valid
+ptb_word_test = ptb.word_test
 
-ptb_char_train_path = "datasets\ptb\ptb.char.train.txt"
-ptb_char_valid_path = "datasets\ptb\ptb.char.valid.txt"
-ptb_word_train_path = "datasets\ptb\ptb.train.txt"
-ptb_word_valid_path = "datasets\ptb\ptb.valid.txt"
-ptb_word_test_path = "datasets\ptb\ptb.test.txt"
+wikitext2 = datasets.Wikitext2()
+wikitext2_train = wikitext2.train
+wikitext2_valid = wikitext2.valid
+wikitext2_test = wikitext2.test
 
-wikitext2_train_path = "datasets\wikitext-2-v1\wikitext-2\wiki.train.tokens"
-wikitext2_valid_path = "datasets\wikitext-2-v1\wikitext-2\wiki.valid.tokens"
-wikitext2_test_path = "datasets\wikitext-2-v1\wikitext-2\wiki.test.tokens"
-
-enwik9_path = "datasets\enwik9\enwik9"
-
-# load datasets
-ptb_char_train = datasets.Dataset(ptb_char_train_path)
-ptb_char_valid = datasets.Dataset(ptb_char_valid_path)
-ptb_word_train = datasets.Dataset(ptb_word_train_path)
-ptb_word_valid = datasets.Dataset(ptb_word_valid_path)
-ptb_word_test = datasets.Dataset(ptb_word_test_path)
-
-wikitext2_train = datasets.Dataset(wikitext2_train_path)
-wikitext2_valid = datasets.Dataset(wikitext2_valid_path)
-wikitext2_test = datasets.Dataset(wikitext2_test_path)
-
-enwik9 = datasets.Dataset(enwik9_path)
-train_offset = 9*10**7
-valid_offset = train_offset + 5*10**6
-test_offset = valid_offset + 5*10**6
-enwik9_train = enwik9.dataset[:train_offset] # first 90 million for training
-enwik9_valid = enwik9.dataset[train_offset: valid_offset] # 5 million for valid
-enwik9_test = enwik9.dataset[valid_offset: test_offset] # 5 million for test
+enwik9 = datasets.Enwik9()
+enwik9_train = enwik9.enwik9_train
+enwik9_valid = enwik9.enwik9_valid
+enwik9_test = enwik9.enwik9_test
+print(enwik9_train[0:100])
+print(enwik9_valid[0:100])
+print(enwik9_test[0:100])
 
 
 
@@ -81,22 +68,22 @@ class Model(object):
         return
 
 
-d = 100
-m = d
-n = 70
-a = 10
-
-x = tf.random.normal([m, a]) # d x a
-h_0 = tf.random.normal([n, a]) # n x a
-
-params = Hyperparameters()
-model = Model(params)
-
-q = [] # m x n, where m == d
-r = [] # n x m, where m == d, it's just transposed Q
-for i in range(model.r):
-    q_tmp, r_tmp = model.matrix_decomposition(m, n)
-    q.append(q_tmp)
-    r.append(r_tmp)
-
-model.mogrify(x, h_0, q, r)
+# d = 100
+# m = d
+# n = 70
+# a = 10
+#
+# x = tf.random.normal([m, a]) # d x a
+# h_0 = tf.random.normal([n, a]) # n x a
+#
+# params = Hyperparameters()
+# model = Model(params)
+#
+# q = [] # m x n, where m == d
+# r = [] # n x m, where m == d, it's just transposed Q
+# for i in range(model.r):
+#     q_tmp, r_tmp = model.matrix_decomposition(m, n)
+#     q.append(q_tmp)
+#     r.append(r_tmp)
+#
+# model.mogrify(x, h_0, q, r)
